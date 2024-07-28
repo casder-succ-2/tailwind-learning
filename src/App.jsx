4fbb53da443d878e13a395b3cf19caa595a7268f
David <davidka907@gmail.com>
2024-07-28 16:47:42 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,122 @@
+function App() {
+  return (
+    <div className="mx-auto grid min-h-[100vh] max-w-[1200px] gap-8 p-8 lg:grid-cols-4 lg:place-content-center">
+      <section className="card bg-purple-600 text-white lg:col-span-2">
+        <div className="card-container-name">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="Daniel"
+            className="card-photo"
+          />
+          <span className="card-name">Daniel</span>
+          <span className="card-edu">Vega</span>
+        </div>
+
+        <p className="card-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+        </p>
+
+        <p className="card-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+          quasi suscipit? Omnis consequuntur iure esse repellendus eaque
+          incidunt modi quo impedit, numquam, odio quaerat molestiae tempora
+          illum! Tempora, magni!
+        </p>
+      </section>
+
+      <section className="card bg-gray-700 text-white ">
+        <div className="card-container-name">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="Daniel"
+            className="card-photo"
+          />
+          <span className="card-name">Papich</span>
+          <span className="card-edu">Arthas</span>
+        </div>
+
+        <p className="card-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+        </p>
+
+        <p className="card-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+          quasi suscipit? Omnis consequuntur iure esse repellendus eaque
+          incidunt modi quo impedit, numquam, odio quaerat molestiae tempora
+          illum! Tempora, magni!
+        </p>
+      </section>
+
+      <section className="card bg-white">
+        <div className="card-container-name">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="Daniel"
+            className="card-photo"
+          />
+          <span className="card-name">Validol</span>
+          <span className="card-edu text-black/50">Vega</span>
+        </div>
+
+        <p className="card-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+        </p>
+
+        <p className="card-desc text-black/50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+          quasi suscipit? Omnis consequuntur iure esse repellendus eaque
+          incidunt modi quo impedit, numquam, odio quaerat molestiae tempora
+          illum! Tempora, magni!
+        </p>
+      </section>
+
+      <section className="card bg-gray-800 text-white lg:col-span-2">
+        <div className="card-container-name">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="Daniel"
+            className="card-photo"
+          />
+          <span className="card-name">Kavo</span>
+          <span className="card-edu">Vega</span>
+        </div>
+
+        <p className="card-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+        </p>
+
+        <p className="card-desc">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+          quasi suscipit? Omnis consequuntur iure esse repellendus eaque
+          incidunt modi quo impedit, numquam, odio quaerat molestiae tempora
+          illum! Tempora, magni!
+        </p>
+      </section>
+
+      <section className="card bg-white text-gray-700 lg:row-span-2 lg:row-start-1 lg:col-start-4">
+        <div className="card-container-name">
+          <img
+            src="https://xsgames.co/randomusers/avatar.php?g=male"
+            alt="Daniel"
+            className="card-photo"
+          />
+          <span className="card-name">Lol</span>
+          <span className="card-edu text-black/50">Vega</span>
+        </div>
+
+        <p className="card-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+        </p>
+
+        <p className="card-desc text-black/50">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem fugit
+          quasi suscipit? Omnis consequuntur iure esse repellendus eaque
+          incidunt modi quo impedit, numquam, odio quaerat molestiae tempora
+          illum! Tempora, magni!
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
